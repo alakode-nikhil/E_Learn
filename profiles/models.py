571354f61d9e6fd20ecea0a592ef6eba8224906d
alakode-nikhil <alakodenikhil@gmail.com>
2024-10-09 19:54:17 +0530

    user_profile_created

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+
+    USER_TYPES = [
+        ('STUDENT','student'),
+        ('TRAINER','trainer'),
+        ('MANAGER','manager')
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10,choices=USER_TYPES)

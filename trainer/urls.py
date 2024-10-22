0b31196752bdf61b71cb4584d8f6d7d884583399
alakode-nikhil <alakodenikhil@gmail.com>
2024-10-23 02:43:50 +0530

    initial trainer views

--- trainer/urls.py
+++ trainer/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.TestView.as_view(), name='trainer_home')
+    path('', views.HomeView.as_view(), name='trainer_home'),
+    path('student_details/<int:course_id>/', views.StudentList.as_view(), name='course_student_list')
 ]
--- trainer/views.py
+++ trainer/views.py
@@ -1,7 +1,39 @@
+from typing import Any
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.contrib.auth.models import User
+from manager.models import Course
+from django.db.models.query import QuerySet
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
-class TestView(TemplateView):
 
-    template_name = 'trainer/test.html'
+class HomeView(LoginRequiredMixin, ListView):
+
+    model = Course
+    template_name = 'trainer/home.html'
+    context_object_name = 'courses'
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Course.objects.filter(trainer = self.request.user)
+    
+    def handle_no_permission(self) -> HttpResponseRedirect:
+        return render(self.request, 'error/denied_access.html', status=403)
+    
+class StudentList(LoginRequiredMixin, ListView):
+
+    model = User
+    template_name = 'trainer/student_list.html'
+    context_object_name = 'students'
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[Any]:
+        course_id = self.kwargs['course_id']
+        course = Course.objects.get(id = course_id)
+        return User.objects.filter(profile__type = 'STUDENT', ispurchased__course = course, ispurchased__is_purchased = True)
+    
+    def handle_no_permission(self) -> HttpResponseRedirect:
+        return render(self.request, 'error/denied_access.html', status=403)

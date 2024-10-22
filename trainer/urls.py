0182c12b89e072202a9859c775c3bb9be2e6a288
alakode-nikhil <alakodenikhil@gmail.com>
2024-10-23 03:43:49 +0530

    add student progress analysis

--- trainer/urls.py
+++ trainer/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     path('', views.HomeView.as_view(), name='trainer_home'),
-    path('student_details/<int:course_id>/', views.StudentList.as_view(), name='course_student_list')
+    path('student_details/<int:course_id>/', views.StudentList.as_view(), name='course_student_list'),
+    path('course_progress/<course_id>/student/<student_id>/', views.student_progress, name ='course_student_progress'),
 ]
--- trainer/views.py
+++ trainer/views.py
@@ -2,10 +2,14 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.models import User
-from manager.models import Course
+from manager.models import Course, ChapterCompleted
 from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+import os
+from django.conf import settings
+from moviepy.editor import VideoFileClip
 
 
 # Create your views here.
@@ -35,5 +39,38 @@ class StudentList(LoginRequiredMixin, ListView):
         course = Course.objects.get(id = course_id)
         return User.objects.filter(profile__type = 'STUDENT', ispurchased__course = course, ispurchased__is_purchased = True)
     
+    def get_context_data(self, **kwargs):
+        course_id = self.kwargs['course_id']
+        course = Course.objects.get(id = course_id)
+        context = super().get_context_data(**kwargs)
+        context['course'] = course
+        return context
+    
     def handle_no_permission(self) -> HttpResponseRedirect:
         return render(self.request, 'error/denied_access.html', status=403)
+    
+@login_required
+def student_progress(request, course_id, student_id):
+
+    course = Course.objects.get(id = course_id)
+    chapters = course.chapter_set.all()
+    student = User.objects.get(id= student_id)
+
+    total_time = 0
+    completed_time = 0
+    for chapter in chapters:
+        chapter_completed,_ = ChapterCompleted.objects.get_or_create(chapter = chapter, student = student)
+        video_path = os.path.join(settings.MEDIA_ROOT, str(chapter.chapter_video))
+        clip = VideoFileClip(video_path)
+        duration = clip.duration
+        total_time += duration
+        if chapter_completed and chapter_completed.completed:
+            completed_time += duration
+
+    try:
+        progress_percentage = (int(completed_time/total_time)) * 100
+       
+    except ZeroDivisionError:
+        return render(request, 'error/zero_division.html')
+
+    return render(request, 'trainer/student_progress.html', {'course':course, 'student':student, 'progress':progress_percentage})
